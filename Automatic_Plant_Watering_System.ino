61f9bd47c28c0143b22965dc547af1c9a6cfad29
Riza Mohamed <90960705+CODEWITHRIZA@users.noreply.github.com>
2022-11-15 22:27:24 +0530

    Add files via upload

---
+++ Automatic_Plant_Watering_System.ino
@@ -0,0 +1,22 @@
+//Automatic Plant Watering System
+//Used materials are Arduino,Relay,WaterPump,Moisture Sensor
+//Created By RUZAIN MOHAMED T / class :- 3
+
+int water; //random variable 
+void setup() {
+  pinMode(3,OUTPUT); //output pin for relay board, this will sent signal to the relay
+  pinMode(6,INPUT); //input pin coming from soil sensor
+}
+
+void loop() { 
+  water = digitalRead(6);  // reading the coming signal from the soil sensor
+  if(water == HIGH) // if water level is full then cut the relay 
+  {
+  digitalWrite(3,LOW); // low is to cut the relay
+  }
+  else
+  {
+  digitalWrite(3,HIGH); //high to continue proving signal and water supply
+  }
+  delay(400); 
+}
